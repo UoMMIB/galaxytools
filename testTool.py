6cdd8ae0cce78cc69d0e363883f9762576038e53
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2019-07-29 09:48:17 +0100

    Typo

--- testTool.py
+++ testTool.py
@@ -32,7 +32,7 @@ def outHTML(outFile, outPath):
         h.write(output)
     
     if not os.path.exists(outPath):
-       os.mkdirs( outPath )
+       os.makedirs( outPath )
     r = requests.get('https://pbs.twimg.com/profile_images/607846881331412992/stXUAIwe_400x400.png')
 
     open(outfig,'wb').write(r.content)
