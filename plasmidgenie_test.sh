6dbbbf9f9aabcd7ea092ce23e5fbe399d7397484
pablocarb <pablocarb@github.com>
2020-02-19 14:12:05 +0000

    Working version of PlasmidGenie

--- plasmidgenie_test.sh
+++ plasmidgenie_test.sh
@@ -5,4 +5,4 @@ python ${PLASMID_GENIE_PATH}/plasmid_genie/client.py $ICE_SERVER $ICE_USERNAME $
 
 python plasmid_genie/client.py  https://ice.synbiochem.co.uk pablo.carbonell@manchester.ac.uk synbiochem2020 synbiochem  /local_tools/galaxytools/dataset_99.dat plasmidgenie_out.zip MlyI
 
-cd /local_tools/galaxytools/code/PlasmidGenieClient; python plasmid_genie/client.py https://ice.synbiochem.co.uk pablo.carbonell__at__manchester.ac.uk synbiochem2020 synbiochem /export/galaxy-central/database/files/000/dataset_99.dat /export/galaxy-central/database/files/000/dataset_108.dat MlyI
+cd /local_tools/galaxytools/code/PlasmidGenieClient; python plasmid_genie/client.py https://ice.synbiochem.co.uk pablo.carbonell@manchester.ac.uk synbiochem2020 synbiochem /export/galaxy-central/database/files/000/dataset_99.dat /export/galaxy-central/database/files/000/dataset_108.dat MlyI
