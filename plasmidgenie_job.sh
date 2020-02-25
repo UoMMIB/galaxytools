ceab4d3f2eec9eb2a4cf80c05bd04426d99453fe
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2020-02-25 16:44:46 +0000

    Updates on PG

--- plasmidgenie_job.sh
+++ plasmidgenie_job.sh
@@ -3,4 +3,4 @@ export PYTHONPATH=$PYTHONPATH:${PLASMID_GENIE_PATH};
 cd ${PLASMID_GENIE_PATH};
 export PG_OUTPUT=/tmp/output.zip
 python ${PLASMID_GENIE_PATH}/plasmid_genie/client.py $ICE_SERVER $ICE_USERNAME $ICE_PASSWORD $ICE_GROUP $INPUT $PG_OUTPUT MlyI 
-python /tools/plasmidgenietool_split.py $PG_OUTPUT $OUTPUT1 $OUTPUT2
+python /tools/plasmidgenietool_split.py --input $PG_OUTPUT --output1 $OUTPUT1 --output2 $OUTPUT2
