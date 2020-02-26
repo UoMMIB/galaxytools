ccf511834d77d401bf3f9925b9264e5ef15746b8
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2020-02-26 14:51:59 +0000

    Test

--- primerTool.py
+++ primerTool.py
@@ -65,8 +65,8 @@ if __name__ == '__main__':
     script, log = configureTool( args )
     logout = open(log, 'w')
     print('Running primers script...')
-    subprocess.call( "cat "+script)
-    subprocess.call( "bash "+script, shell=True, stdout=logout, stderr=logout )
+    subprocess.call( ["cat",script])
+    subprocess.call( ["bash",script], shell=True, stdout=logout, stderr=logout )
     print('Done.')
     os.chdir(os.getenv( 'SBC_ASSEMBLY_PATH' ))
     # Output is generated sbc-assembly root folder, it would be better to make a local copy of the code
